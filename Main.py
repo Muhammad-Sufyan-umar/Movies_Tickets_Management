d95a8c2aae57a19ca7ce5c55c0bb82e874f4b23a
Muhammad-Sufyan-umar <sufyanumer04@gmail.com>
2026-08-20 21:23:10 +0500

    Implement movie management features in Cinema class

--- Main.py
+++ Main.py
@@ -4,6 +4,38 @@ class Cinema:
         self.customers = []
         self.booking = []
 
+    def add_movie(self, movie):
+        self.movies.append(movie)
+        print("Movie added successfully ✅")
+
+
+    def remove_movie(self, movie):
+        self.movies.remove(movie)
+        print("Movie removed successfully ✅")
+
+
+    def search_movie(self, movie_id):
+        if self.movies:
+            for i in self.movies:
+                if i.movie_id == movie_id:
+                    print("Movie found")
+                    i.show_info()
+                    return i
+
+            print("Movie not found..")
+        else:
+            print("No Movies Yet..")
+
+
+    def display_movies(self):
+        if self.movies:
+            for i in self.movies:
+                i.show_info()
+        else:
+            print("No Movies Yet!!!")
+
+	
+    
 
 class Movie:
     def __init__(self, movie_id, movie_name, genre, duration, price):
@@ -13,6 +45,14 @@ class Movie:
         self.duration = duration
         self.price = price
 
+    def show_info(self):
+        print(
+            f"ID : {self.movie_id} | "
+            f"Name: {self.movie_name} | "
+            f"Genre : {self.genre} | "
+            f"Duration : {self.duration} | "
+            f"Price : {self.price}"
+        )
 
 class Customer:
     def __init__(self, customer_id, name, email):
